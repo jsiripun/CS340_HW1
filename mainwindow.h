92d0c3b0c250e95cbe23b1825f4d3694e20945b2
jsiripun <jsiripun@gmail.com>
2013-10-29 13:11:18 -0500

    files

---
+++ mainwindow.h
@@ -0,0 +1,45 @@
+/**
+* @file
+* @author Jed Siripun
+* @version 1.0
+* @section LICENSE
+*  no license necessary
+* @section DESCRIPTION
+*  A small window that creates some useless buttons and checkboxes
+*/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class MainWindow;
+}
+
+/*! \brief Main class
+ *         of my application for project CS340.
+ *
+ *  Inherits for QMainWindow from Qt
+ */
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+     /**
+      * Constructor for MainWindow
+      *
+      * @param parent a parent widget, can be null
+      */
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
